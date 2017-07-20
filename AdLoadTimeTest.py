1203d835932d9f0e6495ed3c65d9973fbdff6f59
Andre Bento <Andre.Bento@performgroup.com>
2017-07-20 13:05:12 +0100

    Fix wrong .py file

--- AdLoadTimeTest.py
+++ AdLoadTimeTest.py
@@ -3,13 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
+
+if len(sys.argv)>1:
+	executable_path = sys.argv[1]
+else:
+	executable_path = "windows/chromedriver.exe"
 
 prefix = raw_input("file prefix:")
 adblock = raw_input("ad block on? [y or n] ")
 iterations = int(raw_input("number of iterations: "))
-site = raw_input("url: ")
+url = raw_input("url: ")
 
-executable_path = "chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = executable_path
 chrome_options = Options()
 adblockprefix="AdBlockOff"
@@ -18,7 +23,7 @@ if adblock=='y':
 	adblockprefix="AdBlockOn"
 	
 driver = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options)
-
+driver.set_window_size(1280, 760)
 
 fName=prefix+"_"+adblockprefix+"_firstPaint_"+str(iterations)
 f2Name=prefix+"_"+adblockprefix+"_loadEventEnd_"+str(iterations)
@@ -28,14 +33,15 @@ f2 = open(f2Name, 'w')
 
 print "Test initiating, this can take a few seconds..."
 if adblock=='y':
-	time.sleep( 15 )
 	driver.execute_script("window.close();")
 	driver.switch_to_window(driver.window_handles[0])
 
 for i in xrange(0,iterations):
 	
 	driver.get(url)
-	time.sleep( 5 )
+	if i==0:
+		print "skiping first request to avoid cache measurement discrepancies"
+		continue
 	t = driver.execute_script("return ((window.chrome.loadTimes().firstPaintTime * 1000 - window.performance.timing.navigationStart))")
 	t2 = driver.execute_script("return ((window.performance.timing.loadEventEnd - window.performance.timing.navigationStart))")
 	print "firstPaint: %d" % t
